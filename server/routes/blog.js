037a6e16fba17230a3e8d968a88be919e09ad784
iradukundairenee <iradukundairenee9@gmail.com>
2020-09-28 12:51:35 +0200

    admin here will be able to add blog

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -19,6 +19,14 @@ const storage = multer.diskStorage({
 
 const upload=multer({storage:storage});
 
+mongoose.connect('mongodb+srv://node-shop:node-shop@node-shop.satrg.mongodb.net/node-shop?retryWrites=true&w=majority',
+
+
+     {useNewUrlParser: true , useUnifiedTopology:true, useCreateIndex: true}
+
+
+ );
+
 
 
 import Blog from '../models/blog'
@@ -27,7 +35,7 @@ const router = express.Router()
 //router.get('/',blogController.getall)
 router.get('/', (req,res,next) => {
     Blog.find()
-    .select('_id blogTitle blogContent blogImage')
+    .select('_id articleTitle articleName blogImage')
     .exec()
     .then(docs => {
         
@@ -36,9 +44,9 @@ router.get('/', (req,res,next) => {
             blogs:docs.map(doc =>{
  
      return {
-             blogTitle:doc. blogTitle,
-             blogContent:doc.blogContent,
-              blogImage:doc.blogImage, 
+            articleTitle:doc.articleTitle,
+            articleName:doc.articleName, 
+            blogImage:doc.blogImage,
              _id:doc._id,
               request:{
                  type:'GET',
@@ -60,13 +68,47 @@ router.get('/', (req,res,next) => {
             });
     })
  });
+ router.post('/', upload.single('blogImage'), (req,res,next) =>{
+
+    console.log(req.file);
+      const blog = new Blog({
+       _id: new mongoose.Types.ObjectId(),
+       articleTitle:req.body.articleTitle,
+       articleName:req.body.articleName,
+       blogImage:req.file.path
+      });
+      blog
+      .save()
+      .then(result => {
+          console.log(result);
+           res.status(201).json({
+          message:"blog created success",
+          createdBlog:{
+              articleTitle:result.articleTitle,
+             articleName:result.articleName,
+             blogImage:result.blogImage,
+            _id:result._id,
+            request:{
+                type:'GET',
+                url:'https/blog/' + result._id
+            }
+          }
+      });
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json({error: err});
+      })
+     
+  
+      });
  
 
  router.get('/:blogId',(req,res,next) => {
       
     const id=req.params.blogId;
     Blog.findById(id)
-    .select('_id blogTitle blogContent blogImage')
+    .select('_id articleTitle articleName blogImage')
     .exec()
     .then(doc => {
         console.log("form database",doc);
@@ -90,7 +132,7 @@ router.delete('/:blogId', (req,res,next) =>{
     .exec()
     .then(result => {
         
-        res.status(200).json(result);
+        res.status(200).json({message:"blog deleted"});
     })
     .catch(error => {
     console.log(error);
